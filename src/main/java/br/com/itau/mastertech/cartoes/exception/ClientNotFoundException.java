6237570b9b290a3095a1eaea13e7ff572f66b75f
Ayrton Saito <tonsaito@Ayrtons-MacBook-Pro.local>
2020-08-10 16:26:58 -0300

    updating exceptions

--- src/main/java/br/com/itau/mastertech/cartoes/exception/ClientNotFoundException.java
+++ src/main/java/br/com/itau/mastertech/cartoes/exception/ClientNotFoundException.java
@@ -3,8 +3,10 @@ package br.com.itau.mastertech.cartoes.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Cliente não encontrado")
 public class ClientNotFoundException extends RuntimeException{
+
+    public ClientNotFoundException(){ super(); }
 
     public ClientNotFoundException(String message){
         super(message);
